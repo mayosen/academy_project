f1a305fa45850ac4d3050e0329672aa7a3c5b854
Yegor <gare577@gmail.com>
2022-09-09 09:31:59 +0300

    Setup project

---
+++ src/test/java/com/mayosen/academy/AcademyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayosen.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
