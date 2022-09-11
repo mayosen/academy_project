18c37fef2792f9e3026500e55678ca8ffcc7ef1b
Yegor <gare577@gmail.com>
2022-09-11 11:16:54 +0300

    Refactor building ResponseEntity. Fix not found status code

--- src/main/java/com/mayosen/academy/controllers/ExceptionAdvice.java
+++ src/main/java/com/mayosen/academy/controllers/ExceptionAdvice.java
@@ -2,6 +2,7 @@ package com.mayosen.academy.controllers;
 
 import com.mayosen.academy.exceptions.ItemNotFoundException;
 import com.mayosen.academy.responses.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,16 +20,16 @@ public class ExceptionAdvice {
             MethodArgumentTypeMismatchException.class
     })
     public ResponseEntity<ErrorResponse> handleValidationException() {
-        return ResponseEntity.badRequest().body(new ErrorResponse(400, "Validation failed"));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(400, "Validation failed"));
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorResponse> handleServiceValidationException(ValidationException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(400, "Validation failed"));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(400, "Validation failed"));
     }
 
     @ExceptionHandler(ItemNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(Exception e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(404, "Item not found"));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(404, "Item not found"));
     }
 }
