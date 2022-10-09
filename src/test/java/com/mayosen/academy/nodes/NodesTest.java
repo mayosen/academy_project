952f464851e335c248db7e522a49c953175bb796
mayosen <gare577@gmail.com>
2022-10-09 17:55:28 +0300

    Fix /nodes test plug

--- src/test/java/com/mayosen/academy/nodes/NodesTest.java
+++ src/test/java/com/mayosen/academy/nodes/NodesTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -11,10 +12,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class NodesTest {
+    private final MockMvc mockMvc;
+
     @Autowired
-    private MockMvc mockMvc;
+    public NodesTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
 
     @Test
+    @Sql("/truncate.sql")
     public void notFound() throws Exception {
         mockMvc
                 .perform(get("/nodes"))
